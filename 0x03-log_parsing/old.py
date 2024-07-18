36182ebb24800f17acd3747965cbbd61b7815150
gnxrlyqf <gnarlydestructor@outlook.com>
2024-07-18 23:36:51 +0100

    0

--- 0x03-log_parsing/old.py
+++ 0x03-log_parsing/old.py
@@ -2,26 +2,27 @@
 """
     Task 0
 """
-
 import sys
-import re
 
 
 def stats(codes, size):
     """
     Prints code dictionary after each 10
     Args:
-        codes: dictionary of status code count
-        size: total file size
+        codes: dictionary of codes
+        size: total size of the files
+    Returns:
+        Nothing
     """
+
     print("File size: {}".format(size))
     for k, v in sorted(codes.items()):
         if v != 0:
             print("{}: {}".format(k, v))
 
 
-count = 0
 size = 0
+count = 0
 codes = {"200": 0,
          "301": 0,
          "400": 0,
@@ -31,24 +32,18 @@ codes = {"200": 0,
          "405": 0,
          "500": 0}
 
-ip = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-datetime = r'(\[(\d{4}-\d{2}-\d{2})\s(\d{2}:\d{2}:\d{2}\.\d{6})\])'
-request = r'"GET \/projects\/260 HTTP\/1\.1"'
-code = r'(\d{3})'
-bytes = r'(\d+)'
-
-pattern = rf'{ip}\s-\s{datetime}\s{request}\s{code}\s{bytes}'
-
 try:
     for line in sys.stdin:
-        if match:
-            match = re.match(pattern, line)
-            codes[match.group(5)] += 1
+        parsed = line.split()[::-1]
+        if len(parsed) > 2:
             count += 1
-            size += int(match.group(6))
-            if count == 10:
+            if count <= 10:
+                size += int(parsed[0])
+                if (parsed[1] in codes.keys()):
+                    codes[parsed[1]] += 1
+            if (count == 10):
                 stats(codes, size)
                 count = 0
 
 finally:
-    stats(codes, size)
+	stats(codes, size)
