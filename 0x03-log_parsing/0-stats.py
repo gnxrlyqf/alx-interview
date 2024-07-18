7547921856cb53c5ad833055a568466eacbc24cd
gnxrlyqf <gnarlydestructor@outlook.com>
2024-07-18 22:18:40 +0100

    0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,52 +3,45 @@
 import sys
 
 
-def print_msg(dict_sc, total_file_size):
+def print_msg(codes, size):
     """
     Method to print
     Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
+        codes: dictionary of codes
+        size: total size of the files
     Returns:
         Nothing
     """
 
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+    print("File size: {}".format(size))
+    for k, v in sorted(codes.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
 
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+size = 0
+count = 0
+codes = {"200": 0,
+         "301": 0,
+         "400": 0,
+         "401": 0,
+         "403": 0,
+         "404": 0,
+         "405": 0,
+         "500": 0}
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
-
-        if len(parsed_line) > 2:
+        parsed = line.split()
+        if len(parsed) > 2:
             counter += 1
-
             if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
+                size += int(parsed[-1])
+                if (parsed[-2] in codes.keys()):
+                    codes[parsed[-2]] += 1
             if (counter == 10):
-                print_msg(dict_sc, total_file_size)
+                print_msg(codes, size)
                 counter = 0
 
 finally:
-    print_msg(dict_sc, total_file_size)
+    print_msg(codes, size)
