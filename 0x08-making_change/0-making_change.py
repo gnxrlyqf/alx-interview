fa246b2dce86465ecac1a73c5877f70f47209708
gnxrlyqf <gnarlydestructor@gmail.com>
2024-09-19 18:47:27 +0100

    done

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+"""makeChange funciton module"""
+
+
+def makeChange(coins, total):
+    """
+    Function that returns the fewest number of coins to make a number
+    """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    result = 0
+    new_coins = sorted(coins, key=None, reverse=True)
+    for coin in new_coins:
+        mod = total % coin
+        result += (total - mod) / coin
+        total = mod
+        if mod == 0:
+            return int(result)
+    return -1
