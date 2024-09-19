0a8dc5fcf15a6d48d52184f7a7541d4e8e20b4e2
gnxrlyqf <gnarlydestructor@gmail.com>
2024-09-19 18:54:29 +0100

    done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,12 +8,14 @@ def makeChange(coins, total):
     """
     if not coins or coins is None:
         return -1
+    if total <= 0:
+        return 0
     result = 0
     new_coins = sorted(coins, key=None, reverse=True)
     for coin in new_coins:
-        if total <= 0:
-            return int(result)
         mod = total % coin
         result += (total - mod) / coin
+        if mod == 0:
+            return int(result)
         total = mod
     return -1
